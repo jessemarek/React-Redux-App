2474ec76a1ccbf66c88a01326110fa18495dee01
unknown <dethgrenade@gmail.com>
2020-05-13 15:45:27 -0700

    Updates

---
+++ precache-manifest.c5a6b8a203b3b04b04f34b65cbdf9e62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73042858eac37211bee365315472dabd",
+    "url": "/React-Redux-App/index.html"
+  },
+  {
+    "revision": "a6eef6fce43c4c219df9",
+    "url": "/React-Redux-App/static/css/main.4a6e2c78.chunk.css"
+  },
+  {
+    "revision": "3d4f9e763ca2c8def723",
+    "url": "/React-Redux-App/static/js/2.40ffa983.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Redux-App/static/js/2.40ffa983.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6eef6fce43c4c219df9",
+    "url": "/React-Redux-App/static/js/main.94348a45.chunk.js"
+  },
+  {
+    "revision": "82d13002d369418b4953",
+    "url": "/React-Redux-App/static/js/runtime-main.39f93c2d.js"
+  }
+]);
